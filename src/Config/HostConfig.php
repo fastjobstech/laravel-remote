1e553a062d23d716e97ac60d4ad62815d2cc55cf
wooiliang <chuawooiliang@gmail.com>
2023-04-18 09:51:13 +0800

    WIP

--- src/Config/HostConfig.php
+++ src/Config/HostConfig.php
@@ -4,12 +4,23 @@ namespace Spatie\Remote\Config;
 
 class HostConfig
 {
+    public string $host;
+    public int $port;
+    public string $user;
+    public string $path;
+    public string $privateKeyPath;
+
     public function __construct(
-        public string $host,
-        public int $port,
-        public string $user,
-        public string $path,
-        public ?string $privateKeyPath = null,
+        string $host,
+        int $port,
+        string $user,
+        string $path,
+        string $privateKeyPath
     ) {
+        $this->host = $host;
+        $this->port = $port;
+        $this->user = $user;
+        $this->path = $path;
+        $this->privateKeyPath = $privateKeyPath;
     }
 }
